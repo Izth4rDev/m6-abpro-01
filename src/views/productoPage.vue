ac8b1c622077c143dd229ac7937404f77dcfab1d
Sappy <macarenaramdohr@gmail.com>
2023-04-06 10:17:36 -0400

    estilos

--- src/views/productoPage.vue
+++ src/views/productoPage.vue
@@ -34,3 +34,51 @@ export default {
 </script>
 
 
+<style>
+card-title{
+padding-top: 40px;
+text-align: center;
+font-family: 'Montserrat', sans-serif;
+}
+
+card-body{
+padding-top: 40px;
+text-align: center;
+font-family: 'Montserrat', sans-serif;
+}
+
+input {
+border: 1px solid #ccc;
+border-radius: 4px;
+padding: 8px;
+width: 100%;
+margin-bottom: 50px;
+font-family: 'Montserrat', sans-serif;
+}
+
+.btn{
+margin-top: 20px;
+margin-bottom: 20px;
+padding: 8px;
+width: 100%;
+}
+
+button {
+background-color: #007bff;
+color: #fff;
+text-decoration: none;
+border-style: none;
+padding: 10px 20px;
+cursor: pointer;
+font-family: 'Montserrat', sans-serif;
+}
+
+button:hover {
+background-color: #0069d9;
+}
+
+.error {
+color: red;
+margin-top: 20px;
+}
+</style>
