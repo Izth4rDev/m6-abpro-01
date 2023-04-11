d488527338cafc69360545535a63cb88e8f6412a
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2023-04-11 09:37:54 -0400

    componente carrito eliminado

--- src/views/productoPage.vue
+++ src/views/productoPage.vue
@@ -4,54 +4,53 @@
         <div class="container h-100 py-5">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-10">
-                        <div class="d-flex justify-content-between align-items-center mb-4">
+                    <div class="d-flex justify-content-between align-items-center mb-4">
                         <h3 class="fw-normal mb-0 text-black">Carrito de compra</h3>
-                        <!-- <div>
-                            <p class="mb-0"><span class="text-muted">Ordenado por:</span> <a href="#!" class="text-body">precio <i
-                                class="fas fa-angle-down mt-1"></i></a></p>
-                        </div> -->
-                        </div>
+                    <!-- <div>
+                        <p class="mb-0"><span class="text-muted">Ordenado por:</span> <a href="#!" class="text-body">precio <i
+                            class="fas fa-angle-down mt-1"></i></a></p>
+                    </div> -->
+                    </div>
 
-                        <div class="card rounded-3 mb-4">
-                            <div v-for="carro in newProducts" :key="carro.id" class="card-body p-4">
-                                <div class="row d-flex justify-content-between align-items-center">
-                                <div class="col-md-2 col-lg-2 col-xl-2">
-                                    <img v-bind:src="carro.image" class="img-fluid rounded-3" alt="Cotton T-shirt">
-                                </div>
-                                <div class="col-md-3 col-lg-3 col-xl-3">
-                                    <p class="lead fw-normal mb-2">{{carro.name}}</p>
-                                    <p><span class="text-muted">Descripcion: </span>{{ carro.description }}</p>
-                                </div>
-                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                    <button class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                    <i class="fas fa-minus"></i>
-                                    </button>
+                    <div class="card rounded-3 mb-4">
+                        <div v-for="carro in newProducts" :key="carro.id" class="card-body p-4">
+                            <div class="row d-flex justify-content-between align-items-center">
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+                                <img v-bind:src="carro.image" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-3">
+                                <p class="lead fw-normal mb-2">{{carro.name}}</p>
+                                <p><span class="text-muted">Descripcion: </span>{{ carro.description }}</p>
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                <i class="fas fa-minus"></i>
+                                </button>
 
-                                    <input id="form1" min="0" name="quantity" v-model="carro.cantidad" type="number"
-                                    class="form-control form-control-sm" />
+                                <input id="form1" min="0" name="quantity" v-model="carro.cantidad" type="number"
+                                class="form-control form-control-sm" />
 
-                                    <button class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                    <i class="fas fa-plus"></i>
-                                    </button>
-                                </div>
-                                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                    <h5 class="mb-0">{{carro.price}}</h5>
-                                </div>
-                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                    <a href="#!" v-on:click="eliminarProducto(carro)" class="text-danger"><i class="fas fa-trash fa-lg"> X </i></a>
-                                </div>
-                                </div>
+                                <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                <i class="fas fa-plus"></i>
+                                </button>
+                            </div>
+                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                <h5 class="mb-0">{{carro.price}}</h5>
+                            </div>
+                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                                <a href="#!" v-on:click="eliminarProducto(carro)" class="text-danger"><i class="fas fa-trash fa-lg"> X </i></a>
+                            </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="card">
+                    <div class="card">
                         <div class="card-body">
                             <button type="button" class="btn btn-warning btn-block btn-lg">Procesar el pago</button>
                         </div>
-                        </div>
-
+                    </div>
                 </div>
             </div>
         </div>
